bbe6d2662ad14c0476aec2a69f5e28b7c3b4c817
NiKanMe <780274505@qq.com>
2023-06-07 03:35:16 +0800

    Site updated: 2023-06-07 03:35:16

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "CCCCCC",
+    "citySize": "16",
+    "aqiColor": "D9D9D9",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "15px 10px 10px 20px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "d64c3cf207bd4bd6994f40381c2d36f7"
+  }
+}
