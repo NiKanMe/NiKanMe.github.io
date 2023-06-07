a56ea20976ff46b29ae313d7f3ce7903e761de10
NiKanMe <780274505@qq.com>
2023-06-07 18:13:20 +0800

    Site updated: 2023-06-07 18:13:20

--- js/weather.js
+++ js/weather.js
@@ -10,13 +10,13 @@ WIDGET = {
     "aqiSize": "16",
     "weatherIconSize": "24",
     "alertIconSize": "18",
-    "margin": "10px 5px 5px 5px",
+    "margin": "10px 500px 5px 5px",
     "shadow": "0",
     "language": "auto",
     "borderRadius": "5",
     "fixed": "false",
-    "vertical": "top",
-    "horizontal": "left",
+    "vertical": "center",
+    "horizontal": "center",
     "key": "d64c3cf207bd4bd6994f40381c2d36f7"
   }
 }
